115174a829db56d2c5faf289ae98409feb77c0b0
Manuel Rivas <manuelrivas.2017.18@gmail.com>
2023-05-13 14:17:55 -0300

    Se agrega botón para dar de baja a las tareas

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ 
+ 
+  <TasksComponent/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import TasksComponent from './components/TasksComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   
+    TasksComponent
   }
 }
 </script>
---
+++ src/components/TasksComponent.vue
@@ -0,0 +1,85 @@
+<template>
+    <h1 v-if="estado">Soy el sistema de tareas</h1>
+    <button v-on:click="estado=!estado">{{ estado ? 'Desaparece' : 'Aparece'}}</button>
+    {{ hola }}
+
+<button v-on:click="hecho = true">
+    Para hacer
+</button>
+<button v-on:click="hecho = false">
+    Realizadas
+</button>
+
+<div v-for="(tar,index) in tareas" :key="index" >
+    <div class="cuadrado" v-if="hecho ? !tar.estado && !tar.dadoDeBaja : tar.estado&&!tar.dadoDeBaja">
+        {{tar.tarea}}
+    <button v-on:click="tar.estado = !tar.estado">
+        {{ hecho ? 'Realizada' : 'Reanudar' }}
+    </button>
+    <button v-on:click="tar.dadoDeBaja = true">
+        Dar de baja
+    </button>
+    </div>
+</div>
+
+<input type="text" v-model="nuevaTarea">
+<button v-on:click="cargar">Crear Tarea</button>
+
+</template>
+
+<script>
+export default {
+  name: 'TasksComponent',
+  data() {
+    return {
+        hola: "Hola como andas?",
+        estado: true,
+        hecho: true,
+        nuevaTarea: '',
+        dadoDeBaja: false,
+        tareas: [
+            {
+                id:0,
+                tarea: "Algo para hacer 1",
+                realizado: false
+            },
+            {
+                id:1,
+                tarea: "Algo para hacer 2",
+                realizado: false
+            },
+            {
+                id:2,
+                tarea: "Algo para hacer 3",
+                realizado: false
+            },
+            {
+                id:3,
+                tarea: "Algo para hacer 4",
+                realizado: false
+            }
+        ]
+
+    }
+ },
+  methods: {
+   cargar : function () {
+    this.tareas.push({
+        id: this.tareas.lenght ? this.tareas.lenght : 0,
+        tarea: this.nuevaTarea,
+        estado: false
+    })
+   }
+  }
+}
+</script>
+
+<style scoped>
+
+.cuadrado {
+    border: solid 2px black;
+    padding: 10px;
+    margin: 5px;
+}
+
+</style>
